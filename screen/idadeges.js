735e53b5676fbf02b48846be062e0fa71b51765b
brunof344 <104033731+brunof344@users.noreply.github.com>
2023-01-26 09:21:57 -0300

    Update idadeges.js

--- screen/idadeges.js
+++ screen/idadeges.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import { Text, View, StyleSheetm, ScrollView} from 'react-native';
-import {SafeAreaView, StyleSheet, TextInput, Button} from 'react-native';
-import Styles from '../css/style'
+import { useState } from 'react';
+import { Text, View, Button, TextInput } from 'react-native';
+import Estilos from '../estilos';
 
-//função para Break
-export const Br  = ()=> <Text>{'\n'}</Text>
+export default function Idadegestacao(){
+  const [dia_ultra, setDiaUltra] = useState('');
+  const [dia_atual, setDiaAtual]= useState('');
+  const [idade, setIdade] = useState('');
 
-export function Idade_Gestacional(){
-  return(
-    <View style={Styles.container}>
-      <Text style={Styles.paragraph,{}}></Text>
-      <Text style={Styles.paragraph,{}}></Text>
-      <Text style={Styles.paragraph,{}}></Text>
+  const calcularIdade = () => setIdade(dia_ultra >0 && dia_atual >0 ? 
+  (parseFloat(dia_atual) - parseFloat(dia_ultra))/7: 'Informe valores positivos diferentes de zero')
+
+  return (
+    <View style={Estilos.container}>
+      <Text style={Estilos.paragraph}> Insira os dados abaixo para calcular a idade gestacional.</Text>
+      <TextInput
+        placeholder="Dia da ultrassonografia"
+        style={Estilos.paragraph} 
+        keyboardType={'numeric'}
+        value={dia_ultra}
+        onChangeText={dia_ultra=>setDiaUltra(dia_ultra)}/>
+      <TextInput
+        placeholder="Dia Atual"
+        style={Estilos.paragraph}
+        keyboardType={'numeric'}
+        value={dia_atual}
+        onChangeText={dia_atual=>setDiaAtual(dia_atual)}/>
+      <Button title='Calcular' onPress={calcularIdade} />
+      <Text style={Estilos.paragraph}>{idade ? `\nIdade gestacional: ${idade}`: ''}</Text>
     </View>
   )
 }
-
-
-
-
-
